c460185eeda6559ea68b4f0d6ba6a2f69cbc2067
nethish <nethish259@gmail.com>
2023-08-20 03:26:00 +0530

    Add tree sitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,7 +4,9 @@ vim.g.mapleader = " "
 require("bootstrap");
 require("lazy").setup("plugins")
 
--- TODO	 do this and that
--- FIXME 
--- 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup()
+  end,
+}
