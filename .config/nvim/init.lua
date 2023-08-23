143430dea727d4fdd4c5c9c10edc8b209b8fd327
nethish <nethish259@gmail.com>
2023-08-23 23:43:11 +0530

    Plugins plugins plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,22 @@
+local opt = vim.opt
+local o = vim.o
 
 vim.g.mapleader = " "
 
-require("bootstrap");
+-- PLUGINS
+require("bootstrap")
 require("lazy").setup("plugins")
+require("keymap")
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+
+o.number = true
+o.relativenumber = true
+o.ignorecase = true
+
+o.autoread = true
+o.autowrite = true
 
---
+++ .config/nvim/lua/keymap.lua
@@ -0,0 +1,31 @@
+
+local keymap = vim.keymap
+
+-- LOCAL FUNCTIONS
+
+-- CONFIG
+
+-- SHORTCUTS
+keymap.set('n', '<leader>rn', ':set rnu!<CR>')
+keymap.set('n', '<leader>ek', ':edit '..vim.fn.stdpath('config')..'/lua/keymap.lua <CR>')
+keymap.set('n', '<leader>ev', ':edit '..vim.fn.stdpath('config')..'/init.lua <CR>')
+keymap.set('n', '<leader>sv', ':source '..vim.fn.stdpath('config')..'/init.lua <CR>')
+keymap.set('n', '<leader>scc', ':source % <CR>')
+keymap.set('n', '<esc>', ':noh <CR>')
+
+keymap.set('n', '<C-J>', '<C-W>j')
+keymap.set('n', '<C-K>', '<C-W>k')
+keymap.set('n', '<C-L>', '<C-W>l')
+keymap.set('n', '<C-H>', '<C-W>h')
+
+-- TELESCOPE
+local builtin = require('telescope.builtin')
+keymap.set('n', '<leader>ff', builtin.find_files, {})
+keymap.set('n', '<leader>fg', builtin.live_grep, {})
+keymap.set('n', '<leader>fb', builtin.buffers, {})
+keymap.set('n', '<leader>fh', builtin.help_tags, {})
+keymap.set('n', '<leader>fm', builtin.marks, {})
+keymap.set('n', '<leader>fch', builtin.command_history, {})
+keymap.set('n', '<leader>frg', builtin.registers, {})
+
+
